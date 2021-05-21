643a1ae179f198e941d52e86afaedeef94dbdac8
Christopher <christopherjkang24@gmail.com>
2021-05-21 14:32:44 -0400

    add redux-persist to store state to local storage and rehydrate

--- crwn-clothing/src/redux/root-reducer.js
+++ crwn-clothing/src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'] // only cart will be persisted
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
 })
+
+// Nested persists
+export default persistReducer(persistConfig, rootReducer);
