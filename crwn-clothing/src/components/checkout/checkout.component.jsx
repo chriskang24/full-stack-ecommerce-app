51fc4dc24d26ad51eb196de9d4181bdd57e7498f
Christopher <christopherjkang24@gmail.com>
2021-05-26 12:41:57 -0400

    add stripe integration pay now

--- crwn-clothing/src/components/checkout/checkout.component.jsx
+++ crwn-clothing/src/components/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import CheckoutItem from '../checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../stripe-button/stripe-button.component';
 
 
 
@@ -33,8 +34,14 @@ const CheckoutPage = ({ cartItems, total }) => (
       )
     }
     <div className="total">
-      <span>Total: {total} </span>
+      <span>Total: ${total} </span>
     </div>
+    <div className='test-warning'>
+      * Please use the following test credit card for payments *
+      <br />
+      4242 4242 4242 4242 - Exp: Any Future Date - CVV: Any 3 Digits
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 )
 
---
+++ crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51Iv74XDiONvdRFiAWaD4I5wUGh0HSPACILQjUnIwEE9JfDmslAeUQOmeXuYxKYluMTOJKXa3T4vkGTSpsvV4AFjz00rMvcX9O3'
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful')
+  }
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Commerce Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+}
+
+export default StripeCheckoutButton;
