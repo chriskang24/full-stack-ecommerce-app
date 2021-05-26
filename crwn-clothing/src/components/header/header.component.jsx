52c46c907ddcc63c5787ee318c3b7315c7a80e58
Christopher <christopherjkang24@gmail.com>
2021-05-26 13:58:56 -0400

    refactor homepage and header components to use styled components

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -9,40 +9,40 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles';
 
 const Header = ({ currentUser, hidden }) => (
 
-  <div className="header">
-    <Link className="logo-container" to="/">
+  <HeaderContainer>
+    <LogoContainer to='/'>
       <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to="/shop">
         SHOP
-      </Link>
-      <Link className="option" to="/contact">
+      </OptionLink>
+      <OptionLink to="/contact">
         CONTACT
-      </Link>
+      </OptionLink>
       {
         currentUser ? (
-          <div
-            className='option'
+          <OptionDiv
             onClick={() => auth.signOut()}
           >
             SIGN OUT
-          </div>
+          </OptionDiv>
         )
           : (
-            <Link className='option' to='/signin'>
+            <OptionLink to='/signin'>
               SIGN IN
-            </Link>
+            </OptionLink>
           )}
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {
       hidden ? null : <CartDropdown />
     }
-  </div>
+  </HeaderContainer>
 )
 
 // createStructuredSelector passes the top level state into each subsequent selector below:
