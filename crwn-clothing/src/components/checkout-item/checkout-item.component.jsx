a0db1234baeea19a81672310fbd1434d9acb2b25
Christopher <christopherjkang24@gmail.com>
2021-06-08 14:27:31 -0400

    removed unused imports

--- crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
+++ crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './checkout-item.styles.scss';
 import { connect } from 'react-redux';
 import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
-import { addItemToCart, removeItemFromCart } from '../../redux/cart/cart.utils';
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
 
