1ff1620d3b3432b0894eb572af83a689d14a9dd4
Christopher <christopherjkang24@gmail.com>
2021-06-09 16:14:03 -0400

    seperating CRA app into client folder, add backend server for stripe payments

--- crwn-clothing/client/src/components/stripe-button/stripe-button.component.jsx
+++ crwn-clothing/client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51Iv74XDiONvdRFiAWaD4I5wUGh0HSPACILQjUnIwEE9JfDmslAeUQOmeXuYxKYluMTOJKXa3T4vkGTSpsvV4AFjz00rMvcX9O3'
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful')
+    axios({
+      url: 'payment',
+      method: 'POST',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment Successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error))
+      alert('There was an issue with your payment. Please check the provided credit card details')
+    })
   }
 
   return (
